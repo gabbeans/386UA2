492296e322dcdd8c386d403a9128bbdbd61a0500
gabbeans <80651364+gabbeans@users.noreply.github.com>
2023-11-05 21:31:58 -0800

    Add files via upload
    Health bar movement handler.

---
+++ FloatingHealthBar.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FloatingHealthBar : MonoBehaviour
+{
+    [SerializeField] private Slider slider;
+    [SerializeField] private Camera cam;
+    [SerializeField] private Transform target;
+
+    public void UpdateHealthBar(float currentValue, float maxValue)
+    {
+        slider.value = currentValue / maxValue;
+    }
+    void Update()
+    {
+        transform.rotation = cam.transform.rotation;
+        transform.position = target.position;
+    }
+}
